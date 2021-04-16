07be25d6faa89ff9226574f8151fe9333079bfcc
Evan Furniss <evanfurniss@gmail.com>
2021-04-15 21:26:46 -0500

    Pushing initial react structure, packages, etc

---
+++ evans-blog/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+//Compiles all information on App and pushes it to the main html file in the public folder
+ReactDOM.render(<App />, document.getElementById('root'));
