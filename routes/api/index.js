c405850fc54f8b15b10e80cf2c0b2d3194fe49b3
Evan Furniss <evanfurniss@gmail.com>
2021-04-18 14:28:06 -0500

    Created generic routes set up, added bootstrap-react, babel-cli, and corrected db name on server.js

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const { get } = require("mongoose");
+// const teamController = require("../../controllers/indexController");
+// gotta change the name to match new controller name
+
+
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,11 @@
-// None for now
+var path = require("path");
+var apiRoutes = require("./api");
+var app = require("express").Router();
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.use("/api", apiRoutes);
+
+module.exports = app;
